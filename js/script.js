dabe46692428551c7c0903270be6b3c0468c5935
Lucas Moyer <Lucas Moyer>
2017-01-08 15:26:20 -0800

    type: feat
    body:
    Now there are two cats users can click on. Also coded so it is easier to add more cats.
    
    footer: Version 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-var numClicked = 0;
-$("#catpic").click(function(){
-    numClicked = numClicked + 1;
-    $('#message').text("Chico has been clicked " +numClicked);
+
+var animals = {
+    "cats": [
+    {
+        "name":"Chico",
+        "img":"images/cat1.jpg"
+    },
+    {
+        "name":"Chewie",
+        "img":"images/cat2.jpg"
+    }]
+}
+
+var index = 1;
+function displayCats() {
+    for (cat in animals.cats){
+        console.log("the cats id is" + (index));
+        $('#cat').prepend(
+            '<img src="'+animals.cats[cat].img+'" id =catpic'+(index) +' style="width:750px;height:500px;">'+
+            '<p id="message'+index+'"> '+ animals.cats[cat].name+' has been clicked 0 times</p>');
+            index = index +1;
+    }
+}
+displayCats();
+
+
+var numClicked1 = 0;
+$("#catpic1").click(function(){
+    numClicked1 = numClicked1 + 1;
+    $('#message1').text("Chico has been clicked " +numClicked1);
 });
+
+var numClicked2 = 0;
+$("#catpic2").click(function(){
+    numClicked2 = numClicked2 + 1;
+    $('#message2').text("Chewie has been clicked " +numClicked2);
+});
+
+var numberOfCats = 1;
