597ac71f9fb1ba5aa3916004194cd13fa38dd695
Lucas Moyer <Lucas Moyer>
2017-01-10 16:53:39 -0800

    type: feat
    body: Adding multiple cats
    
    footer: version 3.0

--- js/script.js
+++ js/script.js
@@ -3,18 +3,62 @@ var animals = {
     "cats": [
     {
         "name":"Chico",
-        "img":"images/cat1.jpg"
+        "img":"images/cat1.jpg",
+        "clicks": 0,
+        "number":1
     },
     {
         "name":"Chewie",
-        "img":"images/cat2.jpg"
+        "img":"images/cat2.jpg",
+        "clicks":0,
+        "number":2
+    },
+    {
+        "name":"Jenssen",
+        "img":"images/cat1.jpg",
+        "clicks":0,
+        "number":3
+    },
+    {
+        "name":"Alex",
+        "img":"images/cat2.jpg",
+        "clicks":0,
+        "number":4
+    },
+    {
+        "name":"Peter",
+        "img":"images/cat1.jpg",
+        "clicks":0,
+        "number":5
     }]
 }
 
+for (var i = 0; i <animals.cats.length; i++){
+    var cat = animals.cats[i];
+    var elem = document.createElement('div');
+    elem.textContent = cat;
+
+    $('#cat').prepend(
+            '<img src="'+cat.img+'" id =catpic'+(cat.number) +' style="width:750px;height:500px;">'+
+            '<p id="message'+cat.number+'"> '+ cat.name+' has been clicked 0 times</p>');
+    document.body.appendChild(elem);
+
+    elem.addEventListener('click', (function(catCopy) {
+        return function() {
+            catCopy.clicks = catCopy.clicks + 1;
+            $('#message'+catCopy.number).text(catCopy.name+" has been clicked " + catCopy.clicks);
+            console.log(catCopy);
+        };
+    })(cat));
+    
+}
+
+/*
 var index = 1;
 function displayCats() {
     for (cat in animals.cats){
-        console.log("the cats id is" + (index));
+        catcopy = animals.cats[cat];
+        //clicker(catcopy);
         $('#cat').prepend(
             '<img src="'+animals.cats[cat].img+'" id =catpic'+(index) +' style="width:750px;height:500px;">'+
             '<p id="message'+index+'"> '+ animals.cats[cat].name+' has been clicked 0 times</p>');
@@ -23,17 +67,17 @@ function displayCats() {
 }
 displayCats();
 
-
-var numClicked1 = 0;
-$("#catpic1").click(function(){
-    numClicked1 = numClicked1 + 1;
-    $('#message1').text("Chico has been clicked " +numClicked1);
-});
-
-var numClicked2 = 0;
-$("#catpic2").click(function(){
-    numClicked2 = numClicked2 + 1;
-    $('#message2').text("Chewie has been clicked " +numClicked2);
-});
-
-var numberOfCats = 1;
+function clicker(cat){
+    console.log("#catpic"+cat.number);
+    ("#catpic"+cat.number).addEventListener('click', function(cat){
+        return function() {
+            console.log(cat.name);
+        };
+    })(cat);
+    
+    $("#catpic"+cat.number).click(function(){
+        cat.clicks = cat.clicks+1;
+        $('#message'+cat.number).text(cat.name+" has been clicked " + cat.clicks);
+    });
+}
+*/
